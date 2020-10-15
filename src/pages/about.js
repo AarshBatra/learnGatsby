5d052aec8ad980c6eaaf0febc32e70b4b4d0a059
AarshBatra <aarshbatra.in@gmail.com>
2020-10-15 11:00:36 +0530

    Test Commit 3

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "../components/header"
+
+export default function About() {
+    return(
+        <div style = {{color:`teal`}}> 
+        <Header headerText = "About Gatsby.js" />
+        <Header headerText = "Another Header" />
+        <p> React is really good </p>
+        </div>
+    )
+}
