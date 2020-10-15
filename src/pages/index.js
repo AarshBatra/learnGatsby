fbb453c481e4de2df4777b9bffd709cf18213782
AarshBatra <aarshbatra.in@gmail.com>
2020-10-15 10:55:35 +0530

    SecondCommit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
-
+import { Link } from "gatsby" 
+import Header from "../components/header"
 export default function Home() {
-  return <div>Hello world!</div>
-}
+  return (
+    <div style = {{color : `purple`}}>
+      <Link to = "/contact/"> Click Here </Link>
+      <Header headerText = "Introduction Page" />
+      <p> What a World? </p>
+      <img src = "https://source.unsplash.com/random/400x100" alt = ""/>
+      </div>
+  )
+} 
